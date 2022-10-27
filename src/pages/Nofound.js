fcd0342d655060085eeb577ca27eb5918233576e
zhraa2 <zhraazhraahgz5@gmai.com>
2022-10-27 14:53:21 +0300

    first commit

---
+++ src/pages/Nofound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nofound = () => {
+    return (
+        <div>
+            <img src="/images/404.jpg" alt="Page not found" />
+        </div>
+    )
+}
+
+export default Nofound
